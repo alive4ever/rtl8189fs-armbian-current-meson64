dc3a75fa47dcdf3c36f2bbb55ef6a74f2edcc00e
alive4ever <alive4ever@users.noreply.github.com>
2025-04-12 05:38:43 +0000

    build: ignore date-time error
    Ignore date-time error using KBUILD_CFLAGS_MODULE

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ git clone --branch=rtl8189fs https://github.com/jwrdegoede/rtl8189ES_linux.git
 cd rtl8189ES_linux
 KVER="$(ls /lib/modules | head -n 1)"
 KSRC="$(find /usr/src/ -maxdepth 1 -type d -name 'linux-headers-*')"
-make -j $(nproc) ARCH=arm64 KSRC="$KSRC"
+make -j $(nproc) ARCH=arm64 KSRC="$KSRC" KBUILD_CFLAGS_MODULE+="-Wno-error=date-time"
 tar --numeric-owner -cf - 8189fs.ko | gzip -n > rtl8189fs-"$KVER".tar.gz
 sudo chmod go+w /tmp/workspace
 cp -v rtl8189fs-"$KVER".tar.gz /tmp/workspace/
