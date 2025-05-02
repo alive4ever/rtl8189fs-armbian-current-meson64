409521de5ac80a94d669b1c685826647fc72b5e2
alive4ever <alive4ever@users.noreply.github.com>
2025-05-02 09:50:38 +0000

    build: use dkms
    Use dkms to create dkms tarball with source and binaries for arm64-meson

--- build.sh
+++ build.sh
@@ -1,13 +1,20 @@
+set -e
+if ! echo "$PATH" | grep sbin ; then
+PATH=$PATH:/usr/sbin:/sbin
+fi
+export DEBIAN_FRONTEND="noninteractive"
 curl -sSL "https://apt.armbian.com/armbian.key" | sudo dd of=/etc/apt/keyrings/armbian.key
 echo "deb [signed-by=/etc/apt/keyrings/armbian.key] https://apt.armbian.com bookworm main bookworm-utils bookworm-desktop" | sudo tee /etc/apt/sources.list.d/armbian.list
 sudo apt update
 sudo apt install -y linux-image-current-meson64 linux-headers-current-meson64
-git clone --branch=rtl8189fs https://github.com/jwrdegoede/rtl8189ES_linux.git
-cd rtl8189ES_linux
+MODVER="0.0.1"
+git clone --branch=rtl8189fs https://github.com/jwrdegoede/rtl8189ES_linux.git rtl8189fs-"$MODVER"
 KVER="$(ls /lib/modules | head -n 1)"
 KSRC="$(find /usr/src/ -maxdepth 1 -type d -name 'linux-headers-*')"
-make -j $(nproc) ARCH=arm64 KSRC="$KSRC" KBUILD_CFLAGS+="-Wno-error=date-time"
-tar --numeric-owner -cf - 8189fs.ko | gzip -n > rtl8189fs-"$KVER".tar.gz
+dkms build -m rtl8189fs/"$MODVER"--dkmstree "$PWD" --sourcetree "$PWD" --kernelsourcedir "$KSRC" -a arm64
+dkms mktarball -m rtl8189fs/"$MODVER"--dkmstree "$PWD" --sourcetree "$PWD" --kernelsourcedir "$KSRC" -a arm64
+OUTFILE="$(find rtl8189fs/$MODVER -name '*.tar.gz')"
+echo "dkms tarball is $OUTFILE"
 sudo chmod go+w /tmp/workspace
-cp -v rtl8189fs-"$KVER".tar.gz /tmp/workspace/
+cp -v "$OUTFILE" /tmp/workspace/
 sudo chmod go-w /tmp/workspace
